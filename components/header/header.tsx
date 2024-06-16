61abcb15a900461f3c49fd8a9494d3106ae1e720
Ng Chee Wee <ng_cheewee@yahoo.com>
2024-06-16 09:26:41 +0800

    first commit

---
+++ components/header/header.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+import Menu from './Menu'
+
+const Header = () => {
+  return <header>
+    <nav>
+      <div className='navbar justify-between bg-base-300'>
+        <Link href="/" className='btn btn-ghost text-lg' >
+          Amazona V2
+        </Link>
+        <Menu />
+      </div>
+    </nav>
+  </header>
+}
+
+export default Header
